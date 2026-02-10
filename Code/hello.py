c3fafcf78aed3927af3a800bc3080ec158df8648
Joe578687 <qwei1@babson.edu>
2026-02-10 11:14:21 -0500

    catch up work

--- Code/hello.py
+++ Code/hello.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+import turtle
+def draw_square(turtle obj, siz=100):
+    """Draw a square with the given size."""
+    for '_' in range(4):
+        turtle_obj.forward(size)
+        turtle_obj.left(90) 
